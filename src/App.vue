17a3d12e57917c0c6954ed4d86b09ba99e21e7a9
Andrey Shurygin <shuanvita@gmail.com>
2026-01-14 14:49:22 +0300

    Installing and setting up Tailwind in the project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
+  <h1 class="flex cursor-pointer items-center justify-around text-2xl leading-10 text-[#333]">
+    You did it!
+  </h1>
+  <p class="flex items-center text-[17px]/[1.5] text-purple-900">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci amet asperiores aspernatur
+    autem beatae commodi consequuntur debitis deserunt expedita fugit molestiae nesciunt nobis
+    obcaecati, pariatur quae quisquam, quo voluptatem.
   </p>
 </template>
-
-<style scoped></style>
