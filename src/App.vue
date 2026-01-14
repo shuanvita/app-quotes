5d80b89d1cc759962ec73f97cd697a979bb60dd4
Andrey Shurygin <shuanvita@gmail.com>
2026-01-14 15:30:32 +0300

    Installing custom fonts in the project

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <h1 class="flex cursor-pointer items-center justify-around text-2xl leading-10 text-[#333]">
     You did it!
   </h1>
-  <p class="flex items-center text-[17px]/[1.5] text-purple-900">
+  <p class="flex items-center text-4xl/normal text-gray-900">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci amet asperiores aspernatur
     autem beatae commodi consequuntur debitis deserunt expedita fugit molestiae nesciunt nobis
     obcaecati, pariatur quae quisquam, quo voluptatem.
