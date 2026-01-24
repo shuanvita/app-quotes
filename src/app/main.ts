19c27effdc2c7611a1e9c891ce6300bbdb550a9f
Andrey Shurygin <shuanvita@gmail.com>
2026-01-24 13:52:18 +0300

    Install Tailwind

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import '@/app/styles/main.css'
 
-import App from './App.vue'
-import router from './router'
+import App from './entrypoint/App.vue'
+import router from '../router'
 
 const app = createApp(App)
 
