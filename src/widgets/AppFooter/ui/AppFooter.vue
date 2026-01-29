c9855d6a3b827b416e901e521e736e12b94fd60b
Andrey Shurygin <shuanvita@gmail.com>
2026-01-29 13:05:40 +0300

    Верстка футера сайта

--- src/widgets/AppFooter/ui/AppFooter.vue
+++ src/widgets/AppFooter/ui/AppFooter.vue
@@ -1,5 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { BaseLink } from '@/shared/ui/BaseLink'
+</script>
 
 <template>
-  <footer>Footer</footer>
+  <footer
+    class="text-secondary-text border-secondary flex items-center justify-between border-t pt-4 text-[12px]"
+  >
+    <div>© 2026 App Quotes</div>
+    <div class="item-center flex gap-4">
+      <BaseLink
+        class="hover:text-primary flex items-center gap-2 duration-200"
+        preIcon="outline/github"
+        iconSize="16"
+        href="https://github.com/shuanvita"
+        >GitHub</BaseLink
+      >
+      <BaseLink
+        class="hover:text-primary flex items-center gap-2 duration-200"
+        preIcon="outline/telegram"
+        iconSize="16"
+        href="https://t.me/anvshurygin"
+        >Telegram</BaseLink
+      >
+      <BaseLink
+        class="hover:text-primary flex items-center gap-2 duration-200"
+        preIcon="outline/linkedin"
+        iconSize="16"
+        href="#"
+        >Linkedin</BaseLink
+      >
+    </div>
+  </footer>
 </template>
